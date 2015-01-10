9e2f056e472ef23765be6c55af7907a16eee6dcb
Taichirou Kosugi <t.kosugi@flama.co.jp>
2015-01-10 19:58:24 +0000

    Cal temputure(Congress...)

--- tempClient/app.js
+++ tempClient/app.js
@@ -1,3 +1,23 @@
 var i2c = require('i2c'),
     mqtt = require('mqtt'),
-    client = mqtt.connect('mqtt://yourdomain:1883');
+//    client = mqtt.connect('mqtt://yourdomain:1883'),
+    ADDRESS = 0x5c,
+    INTERVAL = 60*1000; // mili-sec
+
+    sensor = new i2c(ADDRESS, {device: '/dev/i2c-1'});
+
+    // read from ADT7410
+    readValue = function(callback) {
+      sensor.readBytes(0x00, 2, function(err, data) {
+          var temp, value;
+          temp = (data[0] << 8 | data[1]) >> 3;
+          if (temp >= 4096) {
+          temp -= 8192;
+          }
+          value = temp * 0.0625;
+          console.log("Temperature: " + value + " [Deg. C.]");
+          //callback(value);
+          });
+    };
+
+    readValue(null);

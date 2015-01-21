87ed2fa543705874e19adad7619004fc3878136c
Taichirou Kosugi <t.kosugi@flama.co.jp>
2015-01-21 04:12:51 +0000

    Update mqtt

--- tempClient/app.js
+++ tempClient/app.js
@@ -5,18 +5,20 @@ var i2c = require('i2c'),
     ADDRESS = 0x5c,
     INTERVAL = 60*1000, // mili-sec
     child,
-    flag,
     client,
+    flag,
     ws;
 
     function init(){
       sensor = new i2c(ADDRESS, {device: '/dev/i2c-1'});
-      //client = mqtt.connect('mqtt://yourdomain:1883'),
+      console.log(mqtt);
+      client = mqtt.connect('mqtt://yourdomain:1883');
       ws = new WebSocket('ws://104.155.192.253:3001/');
       ws.on('connect',wsOnconnect);
       ws.on('message',wsOnmessage);
       ws.on('error',wsOnerror);
 //    readValue(null);
+      //client.publish('temp','25');
       console.log("init");
       exec('echo 2=10% > /dev/servoblaster');
     }

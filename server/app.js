8503cc915c6affc2eaedce9266944bbf47388717
Taichirou Kosugi <t.kosugi@flama.co.jp>
2015-01-21 05:45:56 +0000

    Server change

--- server/app.js
+++ server/app.js
@@ -19,8 +19,19 @@ var express = require('express'),
   mqttClient = mqtt.connect(),
   //温度を保存しておく
   temp = 0;
-
 app = express();
+
+mqttClient.subscribe('temp');
+
+
+mqttClient.on('message', function (topic, message) {
+    // message is Buffer
+      console.log(message.toString());
+});
+
+mqttClient.publish('presence', 'Hello mqtt');
+
+//mqttClient.end();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -112,13 +123,6 @@ server2.on('connection',function(client){
       client.send(data);
     });
   });
-});
-
-mqttClient.subscribe('temp');
-
-mqttClient.on('temp',function(data){
-  temp = perseInt(data[2]);
-  console.log(data);
 });
 
 // catch 404 and forward to error handler
